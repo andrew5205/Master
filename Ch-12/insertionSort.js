07c215a530b92704369c9165a16152b9620a2a49
Andrew5205 <andrewchung11@gmail.com>
2020-04-10 08:30:58 -0700

    improvement on Ch-12 insertionSort.js

--- Ch-12/insertionSort.js
+++ Ch-12/insertionSort.js
@@ -2,10 +2,13 @@
 // Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. 
 // It is much less efficient on large lists
 
+// O(N^2)
+
 function insertionSort(arr) {
-    for(var i =0; i<arr.length; i++) {
+    for(var i = 0; i < arr.length; i++) {
         var current = arr[i];
         var j = i-1;
+
         while( j >= 0 && arr[j] > current) {
             var temp = arr[j];
             arr[j] = arr[j+1];
@@ -17,6 +20,28 @@ function insertionSort(arr) {
 }
 
 console.log(insertionSort([5,4,3,2,1]));
+
+
+// ********************************************************************************** //
+function insertionSort(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        var current = arr[i];
+        var j = i-1;
+
+        while( j >= 0 && arr[j] > current) {
+            [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            j--;
+        }
+    }
+    return arr;
+}
+
+console.log(insertionSort([5,4,3,2,1]));
+
+
+
+
+
 
 
 
